173aa37381911ebea2ce2ba8c621a51b6ac13765
viveknalubandu <vivek091@gmail.com>
2019-02-04 17:08:21 -0800

    Update TestWelcome.java

--- src/test/java/com/globex/web/TestWelcome.java
+++ src/test/java/com/globex/web/TestWelcome.java
@@ -36,7 +36,7 @@ public class TestWelcome {
 
     @Test
     public void testWelcome() throws Exception {
-
+        // Add tests 
         this.mockMvc.perform(
                 get("/"))
                 .andDo(print())
